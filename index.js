3f7c48091de139903cb1bdd6bbaff9adb284df7a
Rostyslav Yurchyk <ros_ur@ukr.net>
2019-09-26 15:44:46 +0300

    routes in sepatate files

--- index.js
+++ index.js
@@ -2,51 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const fs = require('fs');
-const port = 6969;
+
+const login = require('./routes/login');
+const registration = require('./routes/registration');
+
 const urlencodedParser = bodyParser.urlencoded({
     extended: false,
 });
 
+const port = 6969;
+
 app.use(bodyParser.json());
-app.use(cors());
 app.use(urlencodedParser);
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
-let count = 0;
-
-fs.readFile('./data.json', 'utf8', (error, data) => {
-    const parseData = JSON.parse(data);
-    count = parseData[parseData.length - 1].id;
-});
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/registration', (req, res) => {
-    res.sendFile(__dirname + '/views/registration.html');
-});
-
-app.post('/registration', (req, res) => {
-    fs.readFile('./data.json', 'utf8', (error, data) => {
-        if (!req.body) return res.sendStatus(400);
-        if (error) {
-            return res.sendStatus(404);
-        } else {
-            const obj = {
-                ...req.body,
-                id: ++count,
-            };
-            const dataParse = JSON.parse(data);
-            const list = [...dataParse, obj];
-            fs.writeFileSync('./list.json', JSON.stringify(list));
-            res.send(list);
-        }
-    });
-});
+app.use('/login', login);
+app.use('/registration', registration);
 
 app.listen(port);
--- routes/registration.js
+++ routes/registration.js
@@ -4,7 +4,11 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + './../views/login.html'));
+    res.sendFile(path.join(__dirname + './../views/registration.html'));
+});
+
+router.post('/', (req, res) => {
+    res.send(req.body);
 });
 
 module.exports = router;
