e0b3ffebe7263c9e45da34a503964e209ef93bfc
fredis0629 <fredis0629@gmail.com>
2019-09-30 15:41:14 +0300

    blur valid on registration

--- static/scripts/helpers/validation.js
+++ static/scripts/helpers/validation.js
@@ -1,20 +1,57 @@
 checkPassword = event => {
-  const reg = "^((?=S*?[A-Z])(?=S*?[a-z])(?=S*?[0-9])(?!S*?[!@#$^&%*()+=-[]/{}|:<>?,. а-яА-Я]).{6,})S$";
-  console.log(event.target.value.match(reg));
-
-  if (!event.target.value.match(reg)) {
+  const reg = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{6,})\S$/;
+  if (!reg.test(event.target.value)) {
     event.target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
-    console.log("disable");
   } else {
-    event.target.parentElement.submitRegistration.setAttribute("disabled", "");
+    event.target.parentElement.submitRegistration.removeAttribute("disabled");
+  }
+};
+checkRepeatPassword = event => {
+  const reg = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{6,})\S$/;
+  if (!reg.test(event.target.value) || event.target.value !== event.target.parentElement.password.value) {
+    event.target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
+  } else {
+    event.target.parentElement.submitRegistration.removeAttribute("disabled");
+  }
+};
+checkName = event => {
+  const reg = /^[a-zA-Z0-9]{1,}$/;
+  if (!reg.test(event.target.value)) {
+    event.target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
+  } else {
+    event.target.parentElement.submitRegistration.removeAttribute("disabled");
+  }
+};
+checkSurname = event => {
+  const reg = /^[a-zA-Z0-9]{1,}$/;
+  if (!reg.test(event.target.value)) {
+    event.target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
+  } else {
+    event.target.parentElement.submitRegistration.removeAttribute("disabled");
+  }
+};
+checkLogin = event => {
+  const reg = /^([a-zA-z])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{5,}$/;
+  if (!reg.test(event.target.value)) {
+    event.target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
+  } else {
+    event.target.parentElement.submitRegistration.removeAttribute("disabled");
+  }
+};
+checkEmail = event => {
+  const reg = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+  if (!reg.test(event.target.value)) {
+    event.target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
+  } else {
+    event.target.parentElement.submitRegistration.removeAttribute("disabled");
   }
 };
 
 validity = form => {
-  // form.name.addEventListener("blur", checkName);
-  // form.surname.addEventListener("blur", checkSurname);
-  // form.login.addEventListener("blur", checkLogin);
+  form.name.addEventListener("blur", checkName);
+  form.surname.addEventListener("blur", checkSurname);
+  form.login.addEventListener("blur", checkLogin);
   form.password.addEventListener("blur", checkPassword);
-  // form.email.addEventListener("blur", checkEmail);
-  // form.repeatPassword.addEventListener("blur", checkRepeatPassword);
+  form.repeatPassword.addEventListener("blur", checkRepeatPassword);
+  form.email.addEventListener("blur", checkEmail);
 };
