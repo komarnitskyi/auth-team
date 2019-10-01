5a979f7e33ced465b8ee91217695adc413a23571
fredis0629 <fredis0629@gmail.com>
2019-10-01 14:06:27 +0300

    error msg

--- routes/registration.js
+++ routes/registration.js
@@ -14,47 +14,57 @@ const sequelize = new Sequelize("JquNDev7GA", "JquNDev7GA", "vYpSRLmr34", {
 
 const Model = Sequelize.Model;
 class Users extends Model {}
-Users.init({
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      is: /^[a-zA-Z0-9]{1,}$/,
+Users.init(
+  {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        is: /^[a-zA-Z0-9]{1,}$/
+      }
+    },
+    surname: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        is: /^[a-zA-Z0-9]{1,}$/
+      }
+    },
+    login: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        is: /^([a-zA-z])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{5,}$/
+      },
+      unique: {
+        args: true,
+        msg: "Login already used!"
+      }
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        is: /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{6,})\S$/
+      }
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      },
+      unique: {
+        args: true,
+        msg: "Email address already in use!"
+      }
     }
   },
-  surname: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      is: /^[a-zA-Z0-9]{1,}$/,
-    }
-  },
-  login: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      is: /^([a-zA-z])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{5,}$/,
-    }
-
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      is: /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{6,})\S$/
-    }
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      isEmail: true
-    }
+  {
+    sequelize,
+    modelName: "users"
   }
-}, {
-  sequelize,
-  modelName: "users"
-});
+);
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "./../views/registration.html"));
@@ -62,31 +72,19 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   if (req.body === null) return res.status(400).end();
-  Users.findAll({
-    where: {
-      login: req.body.login
-    }
-  }).then(login => {
-    if (!login.length) {
-      Users.create({
-          name: req.body.name,
-          surname: req.body.surname,
-          login: req.body.login,
-          password: req.body.password,
-          email: req.body.email
-        })
-        .then(() => {
-          res.status(200).send("Registration success");
-        })
-        .catch(Sequelize.ValidationError, err => {
-          console.log('Validation Error');
-
-          res.status(400).send("Validation error");
-        });
-    } else {
-      res.send("This login is already use.");
-    }
-  });
+  Users.create({
+    name: req.body.name,
+    surname: req.body.surname,
+    login: req.body.login,
+    password: req.body.password,
+    email: req.body.email
+  })
+    .then(() => {
+      res.status(200).send("Registration success");
+    })
+    .catch(Sequelize.ValidationError, err => {
+      res.status(400).send(JSON.stringify({ path: err.errors[0].path, type: err.errors[0].type }));
+    });
 });
 
 module.exports = router;
