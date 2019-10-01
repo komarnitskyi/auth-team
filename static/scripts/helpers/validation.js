62bc75e9020062e9364b73ab3e7ee6d702a9b09a
fredis0629 <fredis0629@gmail.com>
2019-10-01 11:43:43 +0300

    full validation on client

--- static/scripts/helpers/validation.js
+++ static/scripts/helpers/validation.js
@@ -1,43 +1,66 @@
+let validityObj = {
+  name: false,
+  surname: false,
+  login: false,
+  email: false,
+  password: false,
+  repeatPassword: false
+};
+
 checkPassword = event => {
   const reg = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{6,})\S$/;
-  disabledBtn(event, reg);
+  disabledBtn(event.target, reg);
+  checkRepeatPassword(event.target.parentElement.repeatPassword);
 };
-checkRepeatPassword = event => {
+checkRepeatPassword = target => {
   const reg = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{6,})\S$/;
-  if (!reg.test(event.target.value) || event.target.value !== event.target.parentElement.password.value) {
-    event.target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
+  if (!reg.test(target.value) || target.value !== target.parentElement.password.value) {
+    target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
+    target.classList.add("validation-error");
+    validityObj[target.name] = false;
   } else {
-    event.target.parentElement.submitRegistration.removeAttribute("disabled");
+    validityObj[target.name] = true;
+    target.classList.remove("validation-error");
+    isValidity(JSON.stringify(validityObj));
   }
 };
 checkNames = event => {
   const reg = /^[a-zA-Z0-9]{1,}$/;
-  disabledBtn(event, reg)
+  disabledBtn(event.target, reg);
 };
 
 checkLogin = event => {
   const reg = /^([a-zA-z])(?!\S*?[\(\)\{\}\/\\\[\],. а-яА-Я]).{5,}$/;
-  disabledBtn(event, reg)
+  disabledBtn(event.target, reg);
 };
 
 checkEmail = event => {
   const reg = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
-  disabledBtn(event, reg)
+  disabledBtn(event.target, reg);
 };
 
-disabledBtn = (event, reg) => {
-  if (!reg.test(event.target.value)) {
-    event.target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
+disabledBtn = (target, reg) => {
+  if (!reg.test(target.value)) {
+    target.parentElement.submitRegistration.setAttribute("disabled", "disabled");
+    target.classList.add("validation-error");
+    validityObj[target.name] = false;
   } else {
+    validityObj[target.name] = true;
+    target.classList.remove("validation-error");
+    isValidity(JSON.stringify(validityObj));
+  }
+};
+isValidity = str => {
+  if (!str.includes("false")) {
     event.target.parentElement.submitRegistration.removeAttribute("disabled");
   }
-}
+};
 
 validity = form => {
   form.name.addEventListener("blur", checkNames);
   form.surname.addEventListener("blur", checkNames);
   form.login.addEventListener("blur", checkLogin);
   form.password.addEventListener("blur", checkPassword);
-  form.repeatPassword.addEventListener("blur", checkRepeatPassword);
+  form.repeatPassword.addEventListener("blur", event => checkRepeatPassword(event.target));
   form.email.addEventListener("blur", checkEmail);
 };
