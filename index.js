f9348ecce576dd88ba518c4a18b2840254f598d8
fredis0629 <fredis0629@gmail.com>
2019-10-10 15:08:46 +0300

    add /users

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const bodyParser = require("body-parser");
 const login = require("./routes/login");
 const registration = require("./routes/registration");
 const cabinet = require("./routes/cabinet");
+const cors = require("cors");
+const users = require("./routes/users");
+
 const app = express();
 const port = 6969;
 
+app.use(
+  cors({
+    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
+    methods: [`GET`, `POST`, `PATCH`, `DELETE`]
+  })
+);
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -18,6 +28,7 @@ app.use("/static", express.static(path.join(__dirname, "static")));
 app.use("/registration", registration);
 app.use("/login", login);
 app.use("/cabinet", cabinet);
+app.use("/users", users);
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
 app.get("/success", (req, res) => res.sendFile(path.join(__dirname + "/views/success.html")));
